1a9019b0bba8f2135640ec1559acdbd2f00fbf57
spring-dev-deepak <65223428+spring-dev-deepak@users.noreply.github.com>
2020-09-25 21:15:28 +0530

    new profile

--- script1.js
+++ script1.js
@@ -2,31 +2,31 @@ const businessNewsApiUrl = 'https://api.nytimes.com/svc/topstories/v2/business.j
 const sportsNewsApiUrl = 'https://api.nytimes.com/svc/topstories/v2/sports.json?api-key='
 const apiKey = '22rCdhycw7PDC0PvAcnKIVo0oFGAQLuX'
 
-// fetch(businessNewsApiUrl + apiKey).then(response => {
-//     response.json().then(data => {
-//         const newsData = data.results;
-//         var news;
+fetch(businessNewsApiUrl + apiKey).then(response => {
+    response.json().then(data => {
+        const newsData = data.results;
+        var news;
 
-//         const newsUpdate1 = document.getElementById('news-update1')
+        const newsUpdate1 = document.getElementById('news-update1')
 
-//         for(news of newsData){
-//            newsUpdate1.innerHTML = newsUpdate1.innerHTML + news.title + "<br><br>"
-//         }
-//     })
-// })
+        for(news of newsData){
+           newsUpdate1.innerHTML = newsUpdate1.innerHTML + news.title + "<br><br>"
+        }
+    })
+})
 
-// fetch(sportsNewsApiUrl + apiKey).then(response => {
-//     response.json().then(data => {
-//         const newsData = data.results;
-//         var news;
+fetch(sportsNewsApiUrl + apiKey).then(response => {
+    response.json().then(data => {
+        const newsData = data.results;
+        var news;
 
-//         const newsUpdate2 = document.getElementById('news-update2')
+        const newsUpdate2 = document.getElementById('news-update2')
 
-//         for(news of newsData){
-//            newsUpdate2.innerHTML = newsUpdate2.innerHTML + news.title + "<br><br>"
-//         }
-//     })
-// })
+        for(news of newsData){
+           newsUpdate2.innerHTML = newsUpdate2.innerHTML + news.title + "<br><br>"
+        }
+    })
+})
 
 const signinData = document.querySelector('.signin-form')
 const userData = document.querySelectorAll('input')
